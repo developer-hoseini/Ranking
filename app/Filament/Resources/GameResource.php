44b8f00b9fd62a2f2f0c6cbf37ae4bc046c52d86
Mohsen Farzadipour <onemohsen@gmail.com>
2023-10-19 22:56:50 +0330

    feat: add game model

---
+++ app/Filament/Resources/GameResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GameResource\Pages;
+use App\Models\Game;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Table;
+
+class GameResource extends Resource
+{
+    protected static ?string $model = Game::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required()->maxLength(255),
+                TextInput::make('sort')->numeric()->minValue(1),
+                Checkbox::make('active')->default(true),
+                Checkbox::make('is_online')->default(true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id'),
+                TextColumn::make('name')
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(),
+                TextColumn::make('sort')->toggleable()->searchable(),
+                ToggleColumn::make('active')
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(),
+                TextColumn::make('is_online')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                Filter::make('id'),
+                Filter::make('name'),
+                Filter::make('sort'),
+                Filter::make('active'),
+                Filter::make('is_online'),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGames::route('/'),
+            'create' => Pages\CreateGame::route('/create'),
+            'edit' => Pages\EditGame::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2023_10_19_181721_create_games_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('sort')->default(0);
+            $table->boolean('active')->default(true);
+            $table->boolean('is_online')->default(false);
+            $table->timestamp('image_updated_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
