5a2017ada94a71b402800ca9c82d09ee61ba5f89
Mohsen Farzadipour <onemohsen@gmail.com>
2023-10-20 16:35:59 +0330

    chore: add relation contry and state

--- database/migrations/2023_10_20_115341_create_countries_table.php
+++ database/migrations/2023_10_20_115341_create_countries_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('sortname', 10);
             $table->string('name', 150);
-            $table->smallInteger('phonecode');
+            $table->smallInteger('phonecode')->nullable();
         });
     }
 
