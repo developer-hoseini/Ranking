ac1fd37fe912e31baff44be2dd4aea0ffa8785a4
Mohsen Farzadipour <onemohsen@gmail.com>
2023-10-20 21:00:26 +0330

    feat: add status model

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $statuses = [
+        //     ['name' => 'in club' , 'model_type' => ],
+        //     ['name' => 'with image' , 'model_type' => ],
+        // ];
+    }
+}
