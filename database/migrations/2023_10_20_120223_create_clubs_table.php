f5f14f305dc4e1397b8a43c1392c770d04a6c086
Mohsen Farzadipour <onemohsen@gmail.com>
2023-10-20 16:36:14 +0330

    feat: impelement club

---
+++ database/migrations/2023_10_20_120223_create_clubs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clubs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->foreignId('state_id')->constrained();
+            $table->string('address')->nullable();
+            $table->string('tel', 50)->nullable();
+            $table->boolean('is_rezvani')->default(0);
+            $table->unsignedInteger('coin')->default(0);
+            $table->unsignedSmallInteger('sort')->default(0);
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clubs');
+    }
+};
