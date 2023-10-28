de9f3b90050a0844175d2c2cbecbaf92ae30a100
MahdiBagheri71 <MahdiBagheriVar@gmail.com>
2023-10-28 21:55:24 +0330

    feat: add vite image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //For brevity in this guide, we will disable Laravel's mass assignment protection. Filament only saves valid data to models so the models can be unguarded safely.
         Model::unguard();
+
+        Vite::macro('image', fn ($asset) => $this->asset("resources/images/{$asset}"));
     }
 }
