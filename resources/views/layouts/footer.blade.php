802376c7ef7110f6867104c4a1d6426ba3537686
Mohsen Farzadipour <onemohsen@gmail.com>
2023-11-04 23:22:36 +0330

    chore: remove language in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,18 +1,19 @@
 <div id="footer">
     <div class="rg-logo"><img src="{{ url('assets/img/coin.png') }}" width="60" align="center"></div>
     <div>{{ __('words.Global Center of Players Ranking') }}</div>
-    <div class=" mt-2">|
+    {{-- <div class=" mt-2">|
         @foreach(LaravelLocalization::getLocalesOrder() as $localeCode => $properties)
             <a href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}"
                class="footer-link">{{ $properties['native'] }}</a> |
         @endforeach
-    </div>
+    </div> --}}
     <div class="footer-link">
         <a href="{{ url('') }}" class="footer-link">Ranking of MetaGamesCoin</a>
     </div>
     <div>
         <p> Address: Canyon center, Internet City, sheikh Zayed highway, Dubai, UAE. <br>
-            E-Mail: info@metagamescoin.io </p>
+            <a class="no-underline text-white" href="mailto:info@metagamescoin.io">E-Mail: info@metagamescoin.io</a> 
+        </p>
     </div>
     <div class="reserved">{{ __('words.All rights reserved by Ranking of Rezvani Game.') }}</div>
 </div>
